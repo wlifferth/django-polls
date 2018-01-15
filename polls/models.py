c8b589ef8c0e1e90d8020afe52ad201f655ddf0f
Will Lifferth <wlifferth@icloud.com>
2018-01-15 13:27:34 -0500

    Finished tutorial step 2

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+import datetime
+
 # Create your models here.
 
 class Question(models.Model):
@@ -9,6 +11,8 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -17,6 +21,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
