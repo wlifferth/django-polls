e173a14a5578e415b244b0be9f74301b3ed51039
Will Lifferth <wlifferth@icloud.com>
2018-01-16 15:01:06 -0500

    Completed first half of tutorial step 4

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 # Create your views here.
 
 def index(request):
@@ -17,8 +18,18 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question {}.".format(question_id)
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/results.html', {'question': question })
     return HttpResponse(response)
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     return HttpResponse("You're voting on question {}".format(question_id))
